4b2e533067687f648cbccdab5e34bb81888e98dd
huybui <huybui@aitt.vn>
2020-05-11 22:11:51 +0700

    learn codersx

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,12 +1,12 @@
 const db = require("../db")
 
 module.exports.requireAuth = (req, res, next) => {
-  if(!req.cookies.userId) {
+  if(!req.signedCookies.userId) {
     res.redirect('/auth/login');
     return;
   } 
 
-  let user = db.get('users').find({ id: req.cookies.userId }).value()
+  let user = db.get('users').find({ id: req.signedCookies.userId }).value()
 
   if(!user) {
     res.redirect('/auth/login');
